8bf0db3628531d367f32352a492d6fd1787aef2c
IPrepTech <ipreptech@gmail.com>
2022-08-06 13:08:55 +0530

    Initial spring boot project

---
+++ src/main/java/com/ipreptech/management/models/Student.java
@@ -0,0 +1,13 @@
+package com.ipreptech.management.models;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Student {
+    private String name;
+    private String id;
+    private String email;
+}
